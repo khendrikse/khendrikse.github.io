8100891feddea919b511a71069d63c9584df46e9
khendrikse <30577427+khendrikse@users.noreply.github.com>
2020-07-13 08:48:30 +0200

    more uniform code by replacing some single quotes with double quotes

--- assets/js/post-header.js
+++ assets/js/post-header.js
@@ -1,15 +1,15 @@
 const colors = [
-  '#FFd2E1',
-  '#DAFBF8',
-  '#DADDFB',
-  '#FFFCDD',
-  '#DAEDFB'
+  "#FFd2E1",
+  "#DAFBF8",
+  "#DADDFB",
+  "#FFFCDD",
+  "#DAEDFB"
 ]
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-const postHeader = document.getElementById('post-header-image')
-const headerStyles = postHeader.getAttribute('style');
-postHeader.setAttribute('style', `background-color: ${colors[getRandomInt(5)]}; ${headerStyles}`)
+const postHeader = document.getElementById("post-header-image")
+const headerStyles = postHeader.getAttribute("style");
+postHeader.setAttribute("style", `background-color: ${colors[getRandomInt(5)]}; ${headerStyles}`)
