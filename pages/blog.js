e55d04887c5fba5bcbd9edc0311406f9d5a65fae
khendrikse <k.hendrikse@youngcapital.nl>
2021-05-06 10:31:13 +0200

    quick fixes with eslint

--- pages/blog.js
+++ pages/blog.js
@@ -3,38 +3,38 @@ import Layout from 'components/Layout';
 import PostList from 'components/PostList';
 import styles from 'styles/home.module.scss';
 
-const Index = ({ posts, title }) => {
-  return (
-    <Layout pageTitle={title}>
-      <div className='container'>
-        <h2 className='section__title'>Blog</h2>
-        <PostList posts={posts} />
-      </div>
-    </Layout>
-  );
-};
+const Index = ({ posts, title }) => (
+  <Layout pageTitle={title}>
+    <div className='container'>
+      <h2 className='section__title'>Blog</h2>
+      <PostList posts={posts} />
+    </div>
+  </Layout>
+);
 
 export default Index;
 
 export async function getStaticProps() {
-  const configData = await import(`../siteconfig.json`);
+  const configData = await import('../siteconfig.json');
 
-  const posts = ((context) => {
+  const posts = (context => {
     const keys = context.keys();
     const values = keys.map(context);
 
-    const data = keys.map((key, index) => {
-      const slug = key.replace(/^.*[\\\/]/, '').slice(0, -3);
-      const date = slug.match(/(\d{1,4}([.\--])\d{1,2}([.\--])\d{1,4})/g);
-      const value = values[index];
-      const document = matter(value.default);
-      return {
-        frontmatter: document.data,
-        markdownBody: document.content,
-        slug,
-        date,
-      };
-    }).sort((a, b) => new Date(b.date) - new Date(a.date));
+    const data = keys
+      .map((key, index) => {
+        const slug = key.replace(/^.*[\\\/]/, '').slice(0, -3);
+        const date = slug.match(/(\d{1,4}([.\--])\d{1,2}([.\--])\d{1,4})/g);
+        const value = values[index];
+        const document = matter(value.default);
+        return {
+          frontmatter: document.data,
+          markdownBody: document.content,
+          slug,
+          date
+        };
+      })
+      .sort((a, b) => new Date(b.date) - new Date(a.date));
     return data;
   })(require.context('../posts', true, /\.md$/));
 
@@ -42,7 +42,7 @@ export async function getStaticProps() {
     props: {
       posts,
       title: configData.default.title,
-      description: configData.default.description,
-    },
+      description: configData.default.description
+    }
   };
 }
