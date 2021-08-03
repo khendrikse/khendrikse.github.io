53166fbe4d8173b38914d228430f1844a92350e6
khendrikse <k.hendrikse@youngcapital.nl>
2021-08-03 20:38:27 +0200

    rename work to projects

--- pages/projects.js
+++ pages/projects.js
@@ -3,23 +3,23 @@ import Layout from 'components/Layout';
 import matter from 'gray-matter';
 import ReactMarkdown from 'react-markdown';
 import ProgressiveImage from 'components/ProgressiveImage';
-import styles from 'styles/work.module.scss';
+import styles from 'styles/projects.module.scss';
 
-const Work = ({ title, work = [] }) => (
+const Projects = ({ title, projects = [] }) => (
   <Layout pageTitle={title}>
     <div className='container'>
-      <h2 className='section__title'>Work</h2>
-      <div className={styles.work__card__grid}>
-        {work.map(({ project }) => (
-          <div key={title} className={styles.work__card}>
+      <h2 className='section__title'>Projects</h2>
+      <div className={styles.projects__card__grid}>
+        {projects.map(({ project }) => (
+          <div key={title} className={styles.projects__card}>
             <ProgressiveImage
-              className={styles.work__card__image}
+              className={styles.projects__card__image}
               src={project.image}
             />
-            <div className={styles.work__card__body}>
+            <div className={styles.projects__card__body}>
               <h1>{project.title}</h1>
               <ReactMarkdown source={project.content} escapeHtml={false} />
-              <div className={styles.work__card__link}>
+              <div className={styles.projects__card__link}>
                 <a href={project.link}>Check it out</a>
               </div>
             </div>
@@ -30,14 +30,14 @@ const Work = ({ title, work = [] }) => (
   </Layout>
 );
 
-Work.propTypes = { title: PropTypes.string, work: PropTypes.array };
+Projects.propTypes = { title: PropTypes.string, projects: PropTypes.array };
 
-export default Work;
+export default Projects;
 
 export async function getStaticProps() {
   const configData = await import('../siteconfig.json');
 
-  const work = (context => {
+  const projects = (context => {
     const keys = context.keys();
     const values = keys.map(context);
     const data = keys
@@ -50,11 +50,11 @@ export async function getStaticProps() {
       })
       .sort((a, b) => a.project.order - b.project.order);
     return data;
-  })(require.context('../work', true, /\.md$/));
+  })(require.context('../projects', true, /\.md$/));
 
   return {
     props: {
-      work,
+      projects,
       title: configData.default.title,
       description: configData.default.description
     }
