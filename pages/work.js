9f81cb16489e1bb023967ccb3a58c82528a15979
khendrikse <30577427+khendrikse@users.noreply.github.com>
2021-08-03 20:36:01 +0200

    add all projects

--- pages/work.js
+++ pages/work.js
@@ -1,64 +1,62 @@
 import PropTypes from 'prop-types';
 import Layout from 'components/Layout';
+import matter from 'gray-matter';
+import ReactMarkdown from 'react-markdown';
 import ProgressiveImage from 'components/ProgressiveImage';
 import styles from 'styles/work.module.scss';
 
-const About = ({ title }) => (
+const Work = ({ title, work = [] }) => (
   <Layout pageTitle={title}>
     <div className='container'>
       <h2 className='section__title'>Work</h2>
       <div className={styles.work__card__grid}>
-        <div className={styles.work__card}>
-          <ProgressiveImage
-            className={styles.work__card__image}
-            src='oob-this.png'
-          />
-          <div className={styles.work__card__body}>
-            <h1>Oob this</h1>
-            <p>
-              One of my favorite podcasts 'delete this' inspired me to create a
-              small website that replaces each vowel in a work with 'oob'. It
-              was loads of fun to make and I totally geeked out when they
-              mentioned it in their podcast.
-            </p>
-            <div className={styles.work__card__link}>
-              <a href='https://oob-this.netlify.com/'>Check it out</a>
+        {work.map(({ project }) => (
+          <div key={title} className={styles.work__card}>
+            <ProgressiveImage
+              className={styles.work__card__image}
+              src={project.image}
+            />
+            <div className={styles.work__card__body}>
+              <h1>{project.title}</h1>
+              <ReactMarkdown source={project.content} escapeHtml={false} />
+              <div className={styles.work__card__link}>
+                <a href={project.link}>Check it out</a>
+              </div>
             </div>
           </div>
-        </div>
-        <div className={styles.work__card}>
-          <ProgressiveImage
-            className={styles.work__card__image}
-            src='whatgenre.png'
-          />
-          <div className={styles.work__card__body}>
-            <h1>What Genre</h1>
-            <p>
-              When I first started coding I noticed the importance of building
-              your own projects that you can feel passionate about. What Genre
-              was one of the first projects I did. Using Spotify's API it checks
-              what genre a given artist has.
-            </p>
-            <div className={styles.work__card__link}>
-              <a href='https://whatgenre.herokuapp.com/'>Check it out</a>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   </Layout>
 );
 
-About.propTypes = { title: PropTypes.string };
+Work.propTypes = { title: PropTypes.string, work: PropTypes.array };
 
-export default About;
+export default Work;
 
 export async function getStaticProps() {
   const configData = await import('../siteconfig.json');
 
+  const work = (context => {
+    const keys = context.keys();
+    const values = keys.map(context);
+    const data = keys
+      .map((key, index) => {
+        const value = values[index];
+        const document = matter(value.default);
+        return {
+          project: { ...document.data, content: document.content }
+        };
+      })
+      .sort((a, b) => a.project.order - b.project.order);
+    return data;
+  })(require.context('../work', true, /\.md$/));
+
   return {
     props: {
-      title: configData.default.title
+      work,
+      title: configData.default.title,
+      description: configData.default.description
     }
   };
 }
