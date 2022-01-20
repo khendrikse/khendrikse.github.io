90b429bb9926f080a1b918ee5e12199c59b06cdc
khendrikse <k.hendrikse@youngcapital.nl>
2022-01-20 21:41:50 +0100

    add dark mode

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,46 +1,30 @@
 import styles from './footer.module.scss';
+import DevTo from '../../public/assets/images/devto.svg';
+import GitHub from '../../public/assets/images/github.svg';
+import LinkedIn from '../../public/assets/images/linkedin.svg';
+import Rss from '../../public/assets/images/rss.svg';
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
       <div className={styles.footer__links}>
         <a className={styles.footer__link} href='https://dev.to/khenhey'>
-          <img
-            width='24px'
-            height='24px'
-            src='/assets/images/devto.svg?'
-            alt='DEV.to'
-          />
+          <DevTo width='24px' height='24px' alt='DEV.to' />
         </a>
         <a className={styles.footer__link} href='https://github.com/khendrikse'>
-          <img
-            width='24px'
-            height='24px'
-            src='/assets/images/github.svg?'
-            alt='GitHub'
-          />
+          <GitHub width='24px' height='24px' alt='GitHub' />
         </a>
         <a
           className={styles.footer__link}
           href='https://www.linkedin.com/in/karinhendrikse/'
         >
-          <img
-            width='24px'
-            height='24px'
-            src='/assets/images/linkedin.svg'
-            alt='LinkedIn'
-          />
+          <LinkedIn width='24px' height='24px' alt='LinkedIn' />
         </a>
         <a
           className={styles.footer__link}
           href='https://khendrikse.github.io/feeds/feed.xml'
         >
-          <img
-            width='24px'
-            height='24px'
-            src='/assets/images/rss.svg'
-            alt='RSS'
-          />
+          <Rss width='24px' height='24px' alt='RSS' />
         </a>
         © {new Date().getFullYear()} Karin Hendrikse
       </div>
