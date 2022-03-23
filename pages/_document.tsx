77025285659e08ebdc0b6de9baa0271313757192
khendrikse <30577427+khendrikse@users.noreply.github.com>
2022-03-23 09:27:40 +0100

    add goatcounter

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,6 +20,11 @@ class MyDocument extends Document {
             rel='icon'
             href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌱</text></svg>'
           />
+          <script
+            data-goatcounter='https://khdev.goatcounter.com/count'
+            async
+            src='//gc.zgo.at/count.js'
+          ></script>
         </Head>
         <body>
           <Main />
